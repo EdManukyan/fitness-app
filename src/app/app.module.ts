8ab6e1c0e2357c3162e215c23056c849413160d2
Edik <edik.manukyan@gmail.com>
2019-08-06 16:12:53 -0700

    Feature: SignUp, LogIn and Trainings components were added to the project. Routing module is added and paths are corrected.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,29 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {AppRoutingModule} from './app-routing.module';
 import {MaterialModule} from './material-components/material-module';
 
 import {AppComponent} from './app.component';
+import {SignupComponent} from './auth/signup/signup.component';
+import {LoginComponent} from './auth/login/login.component';
+import {TrainingComponent} from './trainings/training.component';
+import {PastTrainingsComponent} from './trainings/past-trainings/past-trainings.component';
+import {WelcomeComponent} from './welcome/welcome/welcome.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignupComponent,
+    LoginComponent,
+    TrainingComponent,
+    PastTrainingsComponent,
+    WelcomeComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    AppRoutingModule,
     MaterialModule,
   ],
   providers: [],
---
+++ src/app/trainings/training.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-training',
+  templateUrl: './trainings.component.html',
+  styleUrls: ['./trainings.component.css']
+})
+export class TrainingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
